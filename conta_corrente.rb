79b990560d32711c4404b55b13be78dc38b9fb7c
Vitória Renata <vickrenatamartins@gmail.com>
2023-02-21 23:44:41 -0300

    Adicionado o módulo de conta corrente

--- conta_corrente.rb
+++ conta_corrente.rb
@@ -1,10 +1,12 @@
 require_relative 'conta'
 
-class ContasCorrente < Conta
-    def initialize(numero, titular, saldo, limite)
-      @numero = numero 
-      @titular = titular 
-      @saldo = saldo
-      @limite = limite
-    end
+class ContaCorrente < Conta
+  attr_accessor :limite
+
+  def initialize(numero, titular, saldo, limite)
+    @numero = numero 
+    @titular = titular 
+    @saldo = saldo
+    @limite = limite
+  end
 end
